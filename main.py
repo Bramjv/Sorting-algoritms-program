73f516d64fa19d31384d89cc041d4f5496a81ba0
Bramjv <michalraszewski90@wp.pl>
2024-12-24 10:04:51 +0100

    update

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ import heapq
 
 
 # Funkcja do generowania danych
-def generate_data_integer(size, data_type, order="random"):
+def generate_data_integer(size, order="random"):
     data = [random.randint(0, 1000) for _ in range(size)]
-
+    #   data_type = "integer"
     if order == "sorted":
         data.sort()
     elif order == "reverse":
         data.sort(reverse=True)
-    elif order == "partially_sorted":
+    elif order == "partially sorted":
         partially_sorted_size = size // 2
         data[:partially_sorted_size] = sorted(data[:partially_sorted_size])
 
     return data
 
 
-def generate_data_float(size, data_type, order="random"):
+def generate_data_float(size, order="random"):
     data = [random.uniform(0, 1000) for _ in range(size)]
-
+    #    data_type = "float"
     if order == "sorted":
         data.sort()
     elif order == "reverse":
         data.sort(reverse=True)
-    elif order == "partially_sorted":
+    elif order == "partially sorted":
         partially_sorted_size = size // 2
         data[:partially_sorted_size] = sorted(data[:partially_sorted_size])
 
@@ -39,7 +39,7 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     else:
-        pivot = arr[len(arr) // 2]
+        pivot = random.choice(arr)  # Losowy wybór pivota
         left = [x for x in arr if x < pivot]
         middle = [x for x in arr if x == pivot]
         right = [x for x in arr if x > pivot]
@@ -71,7 +71,7 @@ def merge(left, right):
     return result
 
 
-# Implementacja Tim Sort (wbudowany w Pythonie)
+# Implementacja Tim Sort
 def tim_sort(arr):
     arr.sort()  # Python używa Tim Sort wewnętrznie w metodzie .sort()
     return arr
@@ -106,43 +106,6 @@ def bucket_sort(arr):
     return sorted_arr
 
 
-# Implementacja Radix Sort
-def radix_sort(arr):
-    max_num = max(arr)
-    exp = 1
-    while max_num // exp > 0:
-        counting_sort_radix(arr, exp)
-        exp *= 10
-    return arr
-
-
-def counting_sort_radix(arr, exp):
-    output = [0] * len(arr)
-    count = [0] * 10
-
-    # Liczymy wystąpienia dla danego cyfrowego miejsca
-    for num in arr:
-        index = num // exp
-        count[index % 10] += 1
-
-    # Przekształcamy count do pozycji
-    for i in range(1, 10):
-        count[i] += count[i - 1]
-
-    # Budujemy wynik
-    i = len(arr) - 1
-    while i >= 0:
-        num = arr[i]
-        index = num // exp
-        output[count[index % 10] - 1] = num
-        count[index % 10] -= 1
-        i -= 1
-
-    # Kopiujemy posortowane elementy z powrotem
-    for i in range(len(arr)):
-        arr[i] = output[i]
-
-
 # Implementacja Heap Sort
 def heap_sort(arr):
     heapq.heapify(arr)  # Tworzy kopiec
@@ -159,51 +122,90 @@ def measure_sort_time(sort_function, data):
 
 # Główna funkcja testująca i zapisująca wyniki
 def run_tests_and_save_to_csv(output_file):
+    counter = 0
     min_value = 100000
-    max_value = 10000000
-    data_sizes = [random.randint(min_value, max_value) for _ in range(2)]
-    data_characteristics = ["random", "sorted", "reverse", "partially_sorted"]
+    max_value = 1000000
+    data_sizes = [random.randint(min_value, max_value) for _ in range(3)]
+    data_characteristics = ["random", "sorted", "reverse", "partially sorted"]
     sort_algorithms = {
         "Quick Sort": quick_sort,
         "Merge Sort": merge_sort,
         "Tim Sort": tim_sort,
         "Bucket Sort": bucket_sort,
-        "Radix Sort": radix_sort,
         "Heap Sort": heap_sort
     }
 
-    results = []
+    data_characteristics_length = len(data_characteristics)
+    data_sizes_length = len(data_sizes)
+    sort_algorithms_length = len(sort_algorithms)
+    estimated_attempts = data_characteristics_length * data_sizes_length * sort_algorithms_length
+    print(f"Program będzie potrzebował {estimated_attempts} prób aby posortować wszystkie tablice, wszystkimi "
+          f"algorytmami w każdym podanym ułożeniu.")
+
+    results_array = []
 
     with open(output_file, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(["Algorithm", "Data Size", "Data Characteristic", "Time (s)", "Data type(integer/float)"])
+        writer.writerow(
+            ["Proba", "Algorithm", "Data Size", "Data Characteristic", "Time (s)", "Data type(integer/float)"])
 
         for size in data_sizes:
             for characteristic in data_characteristics:
                 for algo_name, algo_func in sort_algorithms.items():
-                    data = generate_data_float(size, data_type = "float", characteristic)
+                    data = generate_data_integer(size, characteristic)
                     time_taken = measure_sort_time(algo_func, data)
-                    writer.writerow([algo_name, size, characteristic, time_taken, data_type])
-                    results.append((algo_name, size, characteristic, time_taken, data_type))
+                    data_type = "integer"
+                    counter += 1
+                    writer.writerow([counter, algo_name, size, characteristic, time_taken, data_type])
+                    results_array.append((counter, algo_name, size, characteristic, time_taken, data_type))
                     print(
-                        f"Algorytm: {algo_name}, Rozmiar: {size}, Charakterystyka: {characteristic}, Czas: {time_taken:.6f} sekund, Typ danych: {data_type}")
+                        f"(Próba: {counter}), Algorytm: {algo_name}, Rozmiar: {size}, Charakterystyka: {characteristic}"
+                        f" ,Czas: {time_taken:.6f} sekund, Typ danych: integer")
+                """ 
+                for algo_name, algo_func in sort_algorithms.items():
+                    data = generate_data_float(size, characteristic)
+                    time_taken = measure_sort_time(algo_func, data)
+                    data_type = "float"
+                    counter += 1
+                    writer.writerow([counter, algo_name, size, characteristic, time_taken, data_type])
+                    results_array.append((counter, algo_name, size, characteristic, time_taken, data_type))
+                    print(
+                        f"(Próba: {counter}), Algorytm: {algo_name}, Rozmiar: {size}, Charakterystyka: {characteristic}"
+                        f" ,Czas: {time_taken:.6f} sekund, Typ danych: float")
+                """
 
-    return results
+    return results_array
 
 
 # Funkcja do wizualizacji wyników
-def visualize_results(results):
-    algorithms = list(set(result[0] for result in results))
-    data_sizes = sorted(list(set(result[1] for result in results)))
-    characteristics = list(set(result[2] for result in results))
+def visualize_results(results_array):
+    algorithms = list(set(result[1] for result in results_array))
+    data_sizes = sorted(list(set(result[2] for result in results_array)))
+    characteristics = list(set(result[3] for result in results_array))
 
     for characteristic in characteristics:
         plt.figure(figsize=(10, 6))
+
+        # Iterujemy przez wszystkie algorytmy
         for algo in algorithms:
-            times = [
-                result[3] for result in results if result[0] == algo and result[2] == characteristic
-            ]
-            plt.plot(data_sizes, times, marker='o', label=algo)
+            times = []
+            # Zbieramy czas dla danego algorytmu i charakterystyki
+            for size in data_sizes:
+                # Wybieramy czas, który odpowiada rozmiarowi danych
+                time_for_size = next(
+                    (result[4] for result in results_array if
+                     result[1] == algo and result[2] == size and result[3] == characteristic),
+                    None
+                )
+                # Dodajemy czas do listy 'times'
+                if time_for_size is not None:
+                    times.append(time_for_size)
+
+            # Sprawdzamy, czy mamy dane do narysowania wykresu
+            if len(times) == len(data_sizes):
+                plt.plot(data_sizes, times, marker='o', label=algo)
+            else:
+                print(f"Brak danych do wykresu dla algorytmu: {algo} i charakterystyki: {characteristic}")
 
         plt.title(f"Czas sortowania dla danych: {characteristic}")
         plt.xlabel("Rozmiar danych")
@@ -215,3 +217,4 @@ def visualize_results(results):
 
 # Uruchomienie testów, zapisanie wyników i wizualizacja
 results = run_tests_and_save_to_csv("sorting_results.csv")
+visualize_results(results)
